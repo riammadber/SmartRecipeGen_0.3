adb652ded27e38ddfd9f46e3825ad2cf7bc88e5d
Md  Riam Madber <ahmedriam00268@gmail.com>
2025-09-29 12:23:15 +0600

    Create client-side files for preview

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-gray-900 mb-4">
+          SmartRecipeGen
+        </h1>
+        <p className="text-lg text-gray-600">
+          Your intelligent recipe generator is ready!
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
